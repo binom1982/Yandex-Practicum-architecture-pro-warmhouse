4f4e38b492468476b9b62da51e25ff4ee898fe15
binom1982 <69306408+binom1982@users.noreply.github.com>
2026-01-03 13:27:34 +0900

    Доработка проекта sensor device

--- services/device-service/Program.cs
+++ services/device-service/Program.cs
@@ -6,57 +6,44 @@ var builder = WebApplication.CreateSlimBuilder(args);
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
 
+});
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.MapOpenApi();
-}
+    app.UseSwaggerUI(c =>
+         c.SwaggerEndpoint("/openapi/v1.json", "API Sensor Service v1")
+    );
+   
+//}
 
-/*Todo[] sampleTodos =
-[
-    new(1, "Walk the dog"),
-    new(2, "Do the dishes", DateOnly.FromDateTime(DateTime.Now)),
-    new(3, "Do the laundry", DateOnly.FromDateTime(DateTime.Now.AddDays(1))),
-    new(4, "Clean the bathroom"),
-    new(5, "Clean the car", DateOnly.FromDateTime(DateTime.Now.AddDays(2)))
-];
-
-
-var todosApi = app.MapGroup("/todos");
-todosApi.MapGet("/", () => sampleTodos)
-        .WithName("GetTodos");
-
-todosApi.MapGet("/{id}", Results<Ok<Todo>, NotFound> (int id) =>
-    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
-        ? TypedResults.Ok(todo)
-        : TypedResults.NotFound())
-    .WithName("GetTodoById");*/
-
-Device[] devices = [
+Sensor[] sensors = [
     new (Guid.NewGuid(), "Датчик температуры","931013150305", "on"),
     new (Guid.NewGuid(), "Датчик отопления", "C8QH6T96DPNG", "on"),
     new (Guid.NewGuid(), "Лампочка", "1002353BVK1990046841" ,"off")
 ];
 
-var devicesApi = app.MapGroup("/devices");
-devicesApi.MapGet("/", () => devices)
-    .WithName("GetAllDevices");
+var sensorsApi = app.MapGroup("/sensors");
+sensorsApi.MapGet("/", () => sensors)
+    .WithName("GetAllSensors");
+
+sensorsApi.MapGet("/{id}", Results<Ok<Sensor>, NotFound> (Guid id) =>
+    sensors.FirstOrDefault(sensor => sensor.Id == id) is { } sensor
+        ? TypedResults.Ok(sensor)
+        : TypedResults.NotFound())
+    .WithName("GetSensorById");
 
 app.Run();
 
 
-public record Device(Guid Id, string Name, string SerialNumber, string Status);
+public record Sensor(Guid Id, string Name, string SerialNumber, string Status);
 
-/*public record Todo(int Id, string? Title, DateOnly? DueBy = null, bool IsComplete = false);
-*/
-[JsonSerializable(typeof(Device[]))]
+[JsonSerializable(typeof(Sensor[]))]
 internal partial class AppJsonSerializerContext : JsonSerializerContext
 {
-
 }
